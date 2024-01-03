0d6d4d59398b23e9e458c31a0a3cc7f90c011c7f
Chris DiStasio <94086919+christopher-distasio@users.noreply.github.com>
2024-01-02 22:50:31 -0600

    Completes ArtistView to render links for albums

--- src/components/ArtistView.js
+++ src/components/ArtistView.js
@@ -1,16 +1,38 @@
 // These components will be making separate API calls from the app
 // component to serve specific data about our artist
 import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-
+import { useParams, Link } from 'react-router-dom'
 function ArtistView() {
     const { id } = useParams()
     const [ artistData, setArtistData ] = useState([])
 
+    useEffect(() => {
+        const API_URL = `http://localhost:4000/album/${id}`
+        const fetchData = async () => {
+            const response = await fetch(API_URL)
+            const resData = await response.json()
+            setArtistData(resData.results)
+        }
+        fetchData()
+    }, [id])
+
+    const justAlbums = artistData.filter(entry => entry.collectionType === 'Album')
+
+    const renderAlbums = justAlbums.map((album, i) => {
+        return (
+            <div key={i}>
+              {/* The key property is missing the {i} */}
+                <Link to={`/album/${album.collectionId}`}>
+                    <p>{album.collectionName}</p>
+                </Link>
+                {/* This final tag is missing the forward slash in source code */}
+            </div>
+        )
+    })
+
     return (
         <div>
-            <h2>The id passed was: {id}</h2>
-            <p>Artist Data Goes Here!</p>
+            {renderAlbums}
         </div>
     )
 }
