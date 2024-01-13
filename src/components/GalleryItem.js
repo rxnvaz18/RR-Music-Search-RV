ecc2aa23daec9c9ebe7bde3e8d7de583af9d9e3e
unknown <roxanvaz31@gmail.com>
2024-01-13 09:17:51 -0800

    fixed funcitonality, assignment complete

--- src/components/GalleryItem.js
+++ src/components/GalleryItem.js
@@ -54,7 +54,6 @@ function GalleryItem(props){
     
     return (
         <div onClick={() =>setView(!view)} style={{'display': 'inline-block'}}>
-            <p>One Gallery Item</p>
                 {view ? detailView() : simpleView()}
         </div>
     )
